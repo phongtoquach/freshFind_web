83b3e9b5da2362590c026303498f6dd182053682
trantantoan <37495549+trantantoan@users.noreply.github.com>
2026-09-24 15:01:21 +0700

    change classname

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import { NavLink } from "react-router-dom";
 
 function Header() {
 
-    const activeClass = ({ isActive }) => (isActive ? "nav-link active" : "nav-link");
+    const activeClass = ({ isActive }) => (isActive ? "link active" : "link");
 
     useEffect(() => {
         console.log("[Header] đang chạy useEffect() của component Header !");
@@ -18,15 +18,15 @@ function Header() {
    
     return (
         <header className="header">
-            <div className="container">
-                <div className="navbar-brand">
-                    <NavLink to="/" className="brand-link">
-                    <span className="brand-mark">FF</span>
-                    <span className="brand-text">FreshFind</span>
+            <div className="box">
+                <div className="brand">
+                    <NavLink to="/" className="brandlink">
+                    <span className="mark">FF</span>
+                    <span className="text">FreshFind</span>
                     </NavLink>
                 </div>
 
-                <div className="navbar-links">
+                <div className="links">
                     <NavLink to="/markets" className={activeClass}>Markets</NavLink>
                     <NavLink to="/produce-guide" className={activeClass}>Produce Guide</NavLink>
                     <NavLink to="/seasonal" className={activeClass}>Seasonal</NavLink>
@@ -34,9 +34,9 @@ function Header() {
                     <NavLink to="/contact" className={activeClass}>Contact</NavLink>
                 </div>
 
-                <div className="navbar-actions">
-                    <NavLink to="/markets" className="market-button">Find a Market</NavLink>
-                    <NavLink to="/login" className="login-link">Login / Sign Up</NavLink>
+                <div className="actions">
+                    <NavLink to="/markets" className="market">Find a Market</NavLink>
+                    <NavLink to="/login" className="login">Login / Sign Up</NavLink>
                 </div>
             </div>
         </header>
