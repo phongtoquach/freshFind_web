5398f217b27f5a037e97abbfe6ae37a96bb78409
trantantoan <37495549+trantantoan@users.noreply.github.com>
2026-09-24 13:40:37 +0700

    toan test produce guide
    toan test produce guide

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './App.css'
-
+import './assets/css/style.css'
+    
 createRoot(document.getElementById('root')).render(
     <App />
 )
