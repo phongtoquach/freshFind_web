7f740e8d6b6b05766e20d3702229a03882cdb641
Phong Quach <phongqt@fpt.com>
2026-09-24 14:15:59 +0700

    add component Header

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,12 @@
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './assets/css/style.css'
-    
+import "./assets/css/style.css";
+
+import App from './App.jsx';
+
 createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
     <App />
+  </BrowserRouter>
 )
