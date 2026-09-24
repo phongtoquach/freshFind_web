3f8aae57935c580b54a6af8a89a9414989e358d6
Phong Quach <phongqt@fpt.com>
2026-09-24 11:22:55 +0700

    up file json

---
+++ src/data/seasons.js
@@ -0,0 +1,42 @@
+[
+    {
+        "id": 1,
+        "name": "Spring",
+        "slug": "spring",
+        "description": "Products that are commonly available during spring.",
+        "startMonth": 1,
+        "endMonth": 3
+    },
+    {
+        "id": 2,
+        "name": "Summer",
+        "slug": "summer",
+        "description": "Products that are commonly available during summer.",
+        "startMonth": 4,
+        "endMonth": 6
+    },
+    {
+        "id": 3,
+        "name": "Autumn",
+        "slug": "autumn",
+        "description": "Products that are commonly available during autumn.",
+        "startMonth": 7,
+        "endMonth": 9
+    },
+    {
+        "id": 4,
+        "name": "Winter",
+        "slug": "winter",
+        "description": "Products that are commonly available during winter.",
+        "startMonth": 10,
+        "endMonth": 12
+    },
+    // {
+    //     "id": 5,
+    //     "name": "Year-round",
+    //     "slug": "year-round",
+    //     "description": "Products that are generally available throughout the year.",
+    //     "startMonth": null,
+    //     "endMonth": null
+    // }
+]
