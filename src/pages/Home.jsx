ce021bee64f184d8595babaefaa5ccd26c28c825
Phong Quach <phongqt@fpt.com>
2026-09-26 01:20:13 +0700

    add component MarketsGrid vao Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,7 @@ import AppContext from "../context/AppContext";
 //import CartContext from "../context/CartContext";
 
 import HomeMarketSearchBox from "../components/HomeMarketSearchBox";
+import MarketsGrid from "../components/MarketsGrid";
 
 function Home() {
     const { userLocation, refreshUserCurrentLocation } = useContext(AppContext);
@@ -37,6 +38,8 @@ function Home() {
             <div style={{ textAlign: "center" }}>Loading...</div>
         )
     }
+
+    const userCurrentLocationObj = {...userLocation};
     
     return (
         <>
@@ -50,53 +53,11 @@ function Home() {
                 <section className="products-section section" id="produce">
                     <div className="container">
                         <div className="section-heading">
-                            <h2 className="section-title">Food Products</h2>
+                            <h2 className="section-title">Nearby Markets</h2>
                         </div>
-                        <div className="product-grid">
-                            <article className="product-card">
-                                <img
-                                alt="Khajur premium dry food"
-                                src="https://images.unsplash.com/photo-1774334136128-bfdecc1c6446?"
-                                />
-                                <h3>Khajur</h3>
-                                <p className="product-category">Fruit</p>
-                                <p className="product-price">$15.00</p>
-                                <p>
-                                Naturally sweet, soft dates packed with energy and essential
-                                nutrients.
-                                </p>
-                                <Link className="text-link">Find at a market <span aria-hidden="true">→</span></Link>
-                            </article>
-                            <article className="product-card">
-                                <img
-                                alt="Qismis premium dry food"
-                                src="https://images.unsplash.com/photo-1600189020840-e9918c25269d?"
-                                />
-                                <h3>Qismis</h3>
-                                <p className="product-category">Fruit</p>
-                                <p className="product-price">$12.00</p>
-                                <p>
-                                Sun-dried raisins with a rich flavor, perfect for snacking and
-                                baking.
-                                </p>
-                                <Link className="text-link">Find at a market <span aria-hidden="true"></span></Link>
-                            </article>
-                            <article className="product-card">
-                                <img
-                                alt="Fig premium dry food"
-                                src="https://images.unsplash.com/photo-1536511397145-ad62741fdf3c?"
-                                />
-                                <h3>Fig</h3>
-                                <p className="product-category">Fruit</p>
-                                <p className="product-price">$18.00</p>
-                                <p>
-                                Premium dried figs, naturally delicious and a wholesome source of
-                                fiber.
-                                </p>
-                                <Link className="text-link"
-                                >Find at a market <span aria-hidden="true">→</span></Link>
-                            </article>
-                        </div>
+
+                        <MarketsGrid sortType="nearest_first" userCurrentLocation={userCurrentLocationObj} />
+
                     </div>
                 </section>
             </main>
