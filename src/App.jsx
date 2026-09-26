82519ca3d85b90516e45194cf2c5cffa24e02ff8
Phong Quach <phongqt@fpt.com>
2026-09-26 12:43:54 +0700

    tam xoa LoginPage

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,6 @@ import ProductsSeasons from "./pages/ProductsSeasons";
 import AboutPage from "./pages/AboutPage.jsx";
 import ContactPage from "./pages/ContactPage.jsx";
 import Bookmarks from "./pages/BookMarks.jsx";
-import LoginPage from "./pages/LoginPage.jsx";
 
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
@@ -39,7 +38,6 @@ function App() {
               <Route path="/products-seasons" element={<ProductsSeasons />} />
               <Route path="/about" element={<AboutPage />} />
               <Route path="/contact" element={<ContactPage />} />
-              <Route path="/login" element={<LoginPage />} />
             </Routes>
 
             <Footer />
